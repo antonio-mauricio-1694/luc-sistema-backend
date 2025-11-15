78cf6893f0493448f58977491835e6c46853c950
antonio-mauricio-1694 <antoniomauriciodev1694@gmail.com>
2025-11-15 15:15:31 -0300

    feat: criação do projeto luc-sistema-backend

---
+++ luc-sistema/src/test/java/br/com/lucsistema/LucSistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucsistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucSistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
