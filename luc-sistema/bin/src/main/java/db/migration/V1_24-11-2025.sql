4d402cc9e25261c8d7a26ab640bee63c30878178
antonio-mauricio-194 <antoniomauriciodev1694@gmail.com>
2026-03-30 19:04:52 -0300

    commit feat: crinado o banco de dados dentro do postgres e integrando nas configuraçoes

---
+++ luc-sistema/bin/src/main/java/db/migration/V1_24-11-2025.sql
@@ -0,0 +1,8 @@
+SELECT constraint_name
+FROM information_schema.constraint_column_usage
+WHERE table_name = 'usuarios_acesso'
+  AND column_name = 'acesso_id'
+  AND constraint_name <> 'unique_acesso_user';
+
+  
+  alter table usuarios_acesso drop CONSTRAINT "ukdoimqmjqv7fcf54tau3bq14xk"
