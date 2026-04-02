a383e25cbf2964d516111be640918053d132f3dd
antonio-mauricio-194 <antoniomauriciodev1694@gmail.com>
2026-04-02 20:11:19 -0300

    commit feat: criacao da classe nota fiscal de compra

--- luc-sistema/src/main/java/br/com/lucsistema/model/ImagemProduto.java
+++ luc-sistema/src/main/java/br/com/lucsistema/model/ImagemProduto.java
@@ -33,7 +33,7 @@ public class ImagemProduto implements  Serializable {
 	 private String imagemMiniatura;
 	
 	@ManyToOne
-	@JoinColumn(name="produto_id" , nullable = false,foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name="pessoa_fk"))
+	@JoinColumn(name="produto_id" , nullable = false,foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name="produto_fk"))
 	private Produto produto;
 
 	public Long getId() {
